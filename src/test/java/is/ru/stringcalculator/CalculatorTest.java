5ffc0ee646c6b18f0fcb8cd56979d6be8442bb88
brynjas <brynja13@ru.is>
2015-10-14 19:46:40 +0000

    Byrjun

---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("src.CalculatorTest");
+    }
+
+	
+
+}
