bfd99de2fb6447e98bffd6c5d6bf8a3d75b306ec
brynjas <brynja13@ru.is>
2015-10-14 21:09:04 +0000

    tekur inn tölur og summar þær

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -8,6 +8,19 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
+		if(text.equals(",")){
+			
+			String [] numbers = text.split(",");
+			
+
+			int result = 0;
+			for(String number : numbers){
+				result += Integer.parseInt(number);
+			}
+			return result;
+			
+		}
+
 		
 		return Integer.parseInt(text);
 
