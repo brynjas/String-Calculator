1890d1d85b97922704e578e4ea088ca6ba8b494d
brynjas <brynja13@ru.is>
2015-10-16 17:20:11 +0000

    kóðinn fyrir Fjórða lið

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,14 +1,37 @@
 package is.ru.stringcalculator;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
+
 	public static int add(String text){
+		//text = "//;\n1,2";
+		//String delim = "!()\\;//-?|^~";
 	
 		if(text.equals("")){
 			return 0;
 		}
 		
+		else if(text.startsWith("//")){
+			
+			String [] textAfter = text.split("\n");
+			//String textAfter = textTemp[1];
+			if(textAfter[1].contains(";")){
+				text = textAfter[1].replace(";",",");
+				return Sum(SplitTheString(text));
+			}
+
+
+			//text = text.replaceFirst("//;\n","");
+			//text = text.replace(";",",");
+			//text = text.replaceAll("[\\s\\;]", "_");
+			//return Sum(SplitTheString(text));
+			//text = text.
+			//text = SplitNewLine(text);
+			 return Sum(SplitTheString(text));
+
+		}
 		else if(text.contains("\n")){
 			
 			text =  SplitNewLine(text);
@@ -25,8 +48,9 @@ public class Calculator {
 			return Integer.parseInt(text);
 		}
 	}
-
-
+	/*private static String[] splitDelimiters(String numbers){
+		return numbers.replace("//;\n1;2" , ",");
+	}*/
 	private static String[] SplitTheString(String numbers){
 		return numbers.split(",");
 	}
