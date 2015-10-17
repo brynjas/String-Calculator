13bd3fe65cb3f1cc45c11d8fd50c1592af136312
brynjas <brynja13@ru.is>
2015-10-17 23:43:59 +0000

    liður 7 virkar

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -9,9 +9,26 @@ public class Calculator {
 			return 0;
 		}
 		else if(text.startsWith("//" ) | text.contains("\n") ){
+			
+			
 			try{
-		
-			 text =  SplitNewLine(splitDelimiters(text));
+			
+			if(text.contains("[")){
+				
+				String textAfter[] = text.split("\n" , 2);
+				int endOFDel = textAfter[0].indexOf(']', 3);
+				
+				String delimiter = textAfter[0].substring(3, endOFDel);
+
+				//String number= text.substring(endOFDel + 2);
+				
+				//String[] splitstring = number.split(delimiter);
+				String numbers[] = textAfter[1].split(delimiter);
+				return Sum(numbers);
+			}
+			else{
+				text =  SplitNewLine(splitDelimiters(text));
+			}	
 			 return Sum(SplitTheString(text));
 
 			}catch (Exception e){
@@ -23,6 +40,22 @@ public class Calculator {
 				return Sum(SplitTheString(text));
 			}
 		}
+	/*private  String splitDelimitersAnyLenght(String numbers){
+    numbers = numbers.split("\n", 2);
+    String result = numbers.replaceAll("[\\*\\-\\+\\.\\^:,?]","");
+    StringBuilder tokens = new StringBuilder(); // here i will save tokens
+    for(int i = 0; i <= numbers.length(); i++){
+        tokens.append(numbers.replaceAll(regexp, ""));
+    }
+    String bla = tokens.toString();
+    bla = bla.substring(2, bla.length());
+    bla = bla.trim();
+    System.out.println(bla);	
+    return bla;
+}*/
+
+			
+	
 	private  String splitDelimiters(String numbers){
 			String [] textAfter = numbers.split("\n");
 			if(textAfter[1].contains(";")){
@@ -30,6 +63,9 @@ public class Calculator {
 			}
 			return numbers;
 	}
+
+
+
 	private  String[] SplitTheString(String numbers){
 		return numbers.split(",");
 	}
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -54,7 +54,6 @@ public class CalculatorTest {
 	}
 	@Test
 	public void testAnyLengthDelimiter() throws Exception {
-		assertEquals(6,calculator.add("//[***]\n1***2***3"));	
+		assertEquals(6,calculator.add("//[###]\n1###2###3"));
 	}
-
 }
